82083950433bcf0a9028eb2b4b3fc9b747add204
river <riverkeh@naver.com>
2023-02-12 19:13:13 +0900

    MODIFY comment

--- BOJ_14501.py
+++ BOJ_14501.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     for i in range(N):
         if i+T[i]<=N: # i일차에 시작하는 작업을 했을 때 N일을 넘어가는지
             dp[i+T[i]]=max(dp[i+T[i]], dp[i]+P[i])
-        dp[i+1]=max(dp[i+1],dp[i]) # 가장 큰 금액 가져가기
+        dp[i+1]=max(dp[i+1],dp[i]) # 가장 큰 금액 가져가기 / N+1 일의 값 계산
 
     print(dp[N])
