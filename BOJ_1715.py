9503aed93842f7dcc4272145dc00e8d62a2cbd6a
river <riverkeh@naver.com>
2023-03-07 20:56:12 +0900

    ADD COMMENT

--- BOJ_1715.py
+++ BOJ_1715.py
@@ -7,15 +7,16 @@ def solve(que):
     result=0
 
     while True:
-        if que.qsize() ==1:
+        if que.qsize() ==1: # 큐의 크기가 1이면 더이상 비교할 값이 없음
             break
 
+        # 최소값 2개를 가져와서 더함
         q1=que.get()
         q2=que.get()
-
         newQ=q1+q2
-        result+=newQ
-        que.put(newQ)
+
+        result+=newQ # 누적합
+        que.put(newQ) # 더한 값을 다시 큐에 넣어줌
 
     return result
 
@@ -23,11 +24,11 @@ def solve(que):
 if __name__=="__main__":
     N = int(sys.stdin.readline())
 
-    cards = PriorityQueue()
+    cards = PriorityQueue() # 우선순위 큐 선언
     for _ in range(N):
         cards.put(int(sys.stdin.readline()))
 
-    if N == 1:
-        print(0)
+    if N == 1: # 1번의 입력만 있는 경우
+        print(0) # 비교할 필요가 없으므로 0 출력
     else:
         print(solve(cards))
